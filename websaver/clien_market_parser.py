7ff8e4c5bdc086d2768c378ef2a183a26d00e5ec
jiwoo.seo <jiwooseo35@gmail.com>
2018-01-13 21:36:51 +0900

    telegrambot

--- websaver/clien_market_parser.py
+++ websaver/clien_market_parser.py
@@ -1,33 +1,35 @@
+
 import requests
 from bs4 import BeautifulSoup
 import os
-
+import time
 import telegram
 
 bot = telegram.Bot(token='539796424:AAF3eiM8S5J2t-5QHRUk1ffZV8EIIlWMTqU')
-chat_id=bot.getUpdates()[-1].message.chat.id
 
+chat_id = bot.getUpdates()[-1].message.chat.id
 
-# 파일의 위치
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-req = requests.get('https://www.clien.net/service/board/sold')
-req.encoding = 'utf-8' 
+while True:
+    req = requests.get('https://clien.net/service/board/sold')
+    req.encoding = 'utf-8'
 
-html = req.text
-soup = BeautifulSoup(html, 'html.parser')
-posts = soup.select('div_content > div:nth-child(10) > div.list_title > a')
-latest = posts[1].text # 0번은 회원중고장터 규칙입니다.
+    html = req.text
+    soup = BeautifulSoup(html, 'html.parser')
+    posts = soup.select('a.list_subject')
+    latest = posts[3].text
 
-with open(os.path.join(BASE_DIR, 'latest.txt'),'r+') as f_read:
-    before = f_read.readline()
-    if before != latest:
-        bot.sendMessage(chat_id=chat_id, text='New posts!!')
-    else:
-        bot.sendMessage(chat_id=chat_id, text='No new post')
+    with open(os.path.join(BASE_DIR, 'latest.txt'), 'r+') as f_read:
+        before = f_read.readline()
+        if before != latest:
+            bot.sendMessage(chat_id=chat_id, text='New Post !! ')
+        else:
+            bot.sendMessage(chat_id=chat_id, text='No Post!! ')
+        f_read.close()
 
-    f_read.close()
-
-with open(os.path.join(BASE_DIR, 'latest.txt'), 'w+') as f_write:
+    with open(os.path.join(BASE_DIR, 'latest.txt'), 'w+') as f_write:
         f_write.write(latest)
         f_write.close()
+
+    time.sleep(60)
